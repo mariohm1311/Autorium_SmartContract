2dc698acaca4b6c88e6eb331d3f7e37052148201
mariohm1311 <mario.he.me3.0@gmail.com>
2017-07-10 19:51:36 +0200

    Updated PoS.sol.

--- PoS.sol
+++ PoS.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.11;
 contract ProofOfStake {
     // variables
 
+    Float Kparam = Float(0,0);
+    Float Kparam_invroot = Float(0,0);
+
     // constructor
     function ProofOfStake() {
     }
@@ -13,6 +16,7 @@ contract ProofOfStake {
         int exp;
     }
 
+    modifier onlyIssuer() { if (msg.sender == issuer) _; }
 
     /*Float block_target = Float(36500000,-5);
     Float bias = Float(10100000,-7);
@@ -21,13 +25,21 @@ contract ProofOfStake {
 
     //Float test0 = sub(target_reward,init_reward);
 
-    Float Kparam = Kparam_calc(Float(36500,-2),Float(101,-2),Float(2,-1),Float(8,-1));
+    //Float Kparam = Kparam_calc(Float(185505800,-2),Float(101,-2),Float(2,-1),Float(8,-1));
     //Float Kparam = Float(71013837700,-3);
-    Float test_reward = reward(Float(36500,-2),Float(101,-2),Kparam,Float(2,-1));
+    //Float test_reward = reward(Float(4*185505800,-2),Float(101,-2),Kparam,Float(2,-1));
     //Float invcube = inv_cuberoot(Float(50,1),Float(12,-2));
     // baked in floatmath
 
     // baked in safemath
+
+    function update_Kparam(int Kparam_data, int Kparam_exp, int Kparam_invroot_data, int Kparam_invroot_exp) onlyIssuer returns(bool) {
+        Kparam.data = Kparam_data;
+        Kparam.exp = Kparam_exp;
+        Kparam_invroot.data = Kparam_invroot_data;
+        Kparam_invroot.exp = Kparam_invroot_exp;
+        return true;
+    }
 
     function prec_check(Float a, int prec) private returns (Float) {
         if (a.exp < -prec) {
@@ -119,7 +131,7 @@ contract ProofOfStake {
         return NR_iter(a,NR_iter(a,NR_iter(a,NR_iter(a,init_guess))));
     }
 
-    function Kparam_calc(Float block_target, Float bias, Float init_reward, Float target_reward) private returns(Float) {
+    /*function Kparam_calc(Float block_target, Float bias, Float init_reward, Float target_reward) private returns(Float) {
         Float memory numerator = mul(sub(bias,init_reward),block_target);
         Float memory denominator = sub(target_reward,init_reward);
         Float memory fraction = div(numerator,denominator);
@@ -127,14 +139,15 @@ contract ProofOfStake {
         Float memory cubed_fraction = mul(fraction,mul(fraction,fraction));
         Float memory cubed_block_target = mul(block_target,mul(block_target,block_target));
         return sub(cubed_fraction,cubed_block_target);
-    }
+    }*/
 
     function give_guess(Float block_num) private returns (Float) {
-        Float memory init_guess = Float(2420000,-9);
+        //Float memory init_guess = Float(47513939800,-17);
         Float memory inv_blocknum = div(Float(1000000000000000,-15),block_num);
-        Float memory init_guess_chk = prec_check(init_guess,8);
+        //Float memory init_guess_chk = prec_check(init_guess,8);
+        Float memory init_guess_chk = prec_check(Kparam_invroot,8)
         Float memory inv_blocknum_chk = prec_check(inv_blocknum,8);
-
+        
         if (inv_blocknum_chk.data < init_guess_chk.data) {
             init_guess = prec_check(inv_blocknum,12);
         }
